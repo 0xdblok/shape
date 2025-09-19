1ef0ed5fd46fb352c62ad05def50b30e9bb93e5f
dblok0x <dblok0x@gmail.com>
2025-09-19 22:36:46 +0200

    ttt

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -6,7 +7,7 @@ export default function Home() {
       <div class="bg-[linear-gradient(rgb(16,16,22),rgb(8,8,11))] box-border caret-transparent flex justify-center min-h-[1000px] overflow-x-hidden overflow-y-auto w-screen py-0 md:py-[50px]">
         <div class="items-center box-border caret-transparent flex flex-col max-w-[1200px] w-full px-5 md:px-[50px]">
           <div class="items-center box-border caret-transparent hidden grid-flow-col grid-cols-[1fr_3fr_1fr] min-h-0 min-w-0 w-full md:grid md:min-h-[auto] md:min-w-[auto]">
-            <a
+            <Link
               href="/"
               class="box-border caret-transparent inline min-h-0 min-w-0 md:block md:min-h-[auto] md:min-w-[auto]"
             >
@@ -20,7 +21,7 @@ export default function Home() {
                   Shaper
                 </p>
               </div>
-            </a>
+            </Link>
             <div class="items-center bg-white/10 box-border caret-transparent gap-x-[normal] flex justify-center justify-self-center min-h-0 min-w-0 gap-y-[normal] border p-2 rounded-full border-solid border-white/10 md:gap-x-[72px] md:min-h-[auto] md:min-w-[auto] md:gap-y-[72px]">
               <astro-island class="box-border caret-transparent contents">
                 <button class="text-gray-400 bg-transparent caret-transparent block min-h-0 min-w-0 outline-transparent outline-offset-2 outline outline-2 text-center border px-4 py-2 rounded-full border-solid border-transparent md:min-h-[auto] md:min-w-[auto] hover:bg-white/10 hover:border-white/10">
@@ -38,12 +39,12 @@ export default function Home() {
                   </div>
                 </button>
               </astro-island>
-              <a
+              <Link
                 href="/faq"
                 class="text-gray-400 box-border caret-transparent block min-h-0 min-w-0 outline-transparent outline-offset-2 outline outline-2 px-4 md:min-h-[auto] md:min-w-[auto] hover:text-white"
               >
                 FAQ
-              </a>
+              </Link>
               <astro-island class="box-border caret-transparent contents">
                 <button class="text-gray-400 bg-transparent caret-transparent block min-h-0 min-w-0 outline-transparent outline-offset-2 outline outline-2 text-center border px-4 py-2 rounded-full border-solid border-transparent md:min-h-[auto] md:min-w-[auto] hover:bg-white/10 hover:border-white/10">
                   <div class="items-center box-border caret-transparent gap-x-2 flex gap-y-2">
@@ -61,18 +62,18 @@ export default function Home() {
                 </button>
               </astro-island>
             </div>
-            <a
+            <Link
               href=""
               class="font-medium content-center bg-[linear-gradient(rgb(255,255,255),rgb(250,250,250))] shadow-[rgba(0,0,0,0)_0px_0px_0px_0px,rgba(0,0,0,0)_0px_0px_0px_0px,rgba(255,255,255,0)_0px_23px_6px_0px,rgba(255,255,255,0.01)_0px_15px_6px_0px,rgba(255,255,255,0.05)_0px_8px_5px_0px,rgba(255,255,255,0.09)_0px_4px_4px_0px,rgba(255,255,255,0.1)_0px_1px_2px_0px] box-border caret-transparent inline h-10 justify-self-end min-h-0 min-w-0 text-center text-nowrap w-fit border px-4 rounded-full border-solid border-transparent md:block md:min-h-[auto] md:min-w-[auto] hover:shadow-[rgba(0,0,0,0)_0px_0px_0px_0px,rgba(0,0,0,0)_0px_0px_0px_0px,rgba(194,208,255,0)_0px_35px_10px_0px,rgba(194,208,255,0.01)_0px_23px_9px_0px,rgba(194,208,255,0.04)_0px_13px_8px_0px,rgba(194,208,255,0.06)_0px_6px_6px_0px,rgba(194,208,255,0.07)_0px_1px_3px_0px] hover:border-white/40"
             >
               <p class="text-transparent bg-clip-text bg-[linear-gradient(90deg,rgb(5,5,5),rgb(21,21,21))] box-border text-nowrap">
                 Coming soon
               </p>
-            </a>
+            </Link>
           </div>
           <astro-island class="box-border caret-transparent contents">
             <div class="items-center box-border caret-transparent flex justify-between min-h-[auto] min-w-[auto] w-full pt-5 md:hidden md:min-h-0 md:min-w-0">
-              <a
+              <Link
                 href="/"
                 class="box-border caret-transparent block min-h-[auto] min-w-[auto] md:inline md:min-h-0 md:min-w-0"
               >
@@ -81,7 +82,7 @@ export default function Home() {
                   alt="Logo"
                   class="aspect-[auto_14_/_26] box-border caret-transparent max-w-full w-3.5"
                 />
-              </a>
+              </Link>
               <button
                 type="button"
                 class="bg-transparent caret-transparent gap-x-0.5 flex flex-col h-3 min-h-[auto] min-w-[auto] outline-transparent outline-offset-2 outline outline-2 gap-y-0.5 text-center w-5 p-0 md:min-h-0 md:min-w-0"
@@ -109,12 +110,12 @@ export default function Home() {
                     every operation is cryptographically proven to be fair and
                     correct. Don’t trust—verify!
                   </p>
-                  <a
+                  <Link
                     href=""
                     class="font-medium bg-[linear-gradient(319deg,rgb(18,20,33)_23.33%,rgb(4,5,18)_76.67%)] bg-origin-border shadow-[rgba(0,0,0,0)_0px_0px_0px_0px,rgba(0,0,0,0)_0px_0px_0px_0px,rgba(255,255,255,0.05)_0px_-2px_20px_0px_inset,rgba(0,0,0,0)_0px_69px_19px_0px,rgba(0,0,0,0.03)_0px_44px_18px_0px,rgba(0,0,0,0.1)_0px_25px_15px_0px,rgba(0,0,0,0.17)_0px_11px_11px_0px,rgba(0,0,0,0.2)_0px_3px_6px_0px] box-border caret-transparent block text-nowrap w-full px-12 py-4 rounded-full border-2 border-solid border-white/10 md:w-fit hover:shadow-[rgba(0,0,0,0)_0px_0px_0px_0px,rgba(0,0,0,0)_0px_0px_0px_0px,rgba(72,78,112,0)_0px_30px_8px_0px,rgba(72,78,112,0.02)_0px_19px_8px_0px,rgba(72,78,112,0.06)_0px_11px_6px_0px,rgba(72,78,112,0.1)_0px_5px_5px_0px,rgba(72,78,112,0.12)_0px_1px_3px_0px]"
                   >
                     Coming soon
-                  </a>
+                  </Link>
                 </div>
               </div>
             </div>
@@ -133,7 +134,7 @@ export default function Home() {
                 </p>
                 <div class="box-border caret-transparent gap-x-6 flex flex-wrap justify-center gap-y-6 md:gap-x-[50px] md:gap-y-[50px]">
                   <div class="items-center box-border caret-transparent flex h-5 justify-center w-[100px] md:h-[50px] md:w-[280px]">
-                    <a
+                    <Link
                       href="https://a16z.com/"
                       class="box-border caret-transparent block"
                     >
@@ -142,10 +143,10 @@ export default function Home() {
                         alt="Andreessen Horowitz"
                         class="box-border caret-transparent max-w-full"
                       />
-                    </a>
+                    </Link>
                   </div>
                   <div class="items-center box-border caret-transparent flex h-5 justify-center w-[100px] md:h-[50px] md:w-[280px]">
-                    <a
+                    <Link
                       href="https://lsvp.com/"
                       class="box-border caret-transparent block"
                     >
@@ -154,7 +155,7 @@ export default function Home() {
                         alt="Lightspeed"
                         class="box-border caret-transparent max-w-full"
                       />
-                    </a>
+                    </Link>
                   </div>
                 </div>
               </div>
@@ -197,26 +198,26 @@ export default function Home() {
             />
             <div class="font-light items-center box-border caret-transparent gap-x-16 flex flex-col gap-y-16 w-full">
               <div class="box-border caret-transparent hidden grid-cols-[repeat(4,minmax(0px,1fr))] min-h-0 min-w-0 gap-y-4 w-full md:grid md:min-h-[auto] md:min-w-[auto]">
-                <a
+                <Link
                   href=""
                   class="box-border caret-transparent inline min-h-0 min-w-0 md:block md:min-h-[auto] md:min-w-[auto]"
                 >
                   Documentation
-                </a>
-                <a
+                </Link>
+                <Link
                   href=""
                   class="box-border caret-transparent inline min-h-0 min-w-0 md:block md:min-h-[auto] md:min-w-[auto]"
                 >
                   Whitepaper
-                </a>
-                <a
+                </Link>
+                <Link
                   href=""
                   class="box-border caret-transparent inline min-h-0 min-w-0 md:block md:min-h-[auto] md:min-w-[auto]"
                 >
                   Shaper Testnet
-                </a>
+                </Link>
                 <div class="items-center box-border caret-transparent gap-x-8 flex justify-end min-h-0 min-w-0 gap-y-8 md:min-h-[auto] md:min-w-[auto]">
-                  <a
+                  <Link
                     href=""
                     class="box-border caret-transparent block min-h-0 min-w-0 md:min-h-[auto] md:min-w-[auto]"
                   >
@@ -225,8 +226,8 @@ export default function Home() {
                       alt="Icon"
                       class="text-gray-400 box-border caret-transparent h-6 w-6"
                     />
-                  </a>
-                  <a
+                  </Link>
+                  <Link
                     href=""
                     class="box-border caret-transparent block min-h-0 min-w-0 md:min-h-[auto] md:min-w-[auto]"
                   >
@@ -235,8 +236,8 @@ export default function Home() {
                       alt="Icon"
                       class="text-gray-400 box-border caret-transparent h-6 w-6"
                     />
-                  </a>
-                  <a
+                  </Link>
+                  <Link
                     href=""
                     class="box-border caret-transparent block min-h-0 min-w-0 md:min-h-[auto] md:min-w-[auto]"
                   >
@@ -245,20 +246,20 @@ export default function Home() {
                       alt="Icon"
                       class="text-gray-400 box-border caret-transparent h-6 w-6"
                     />
-                  </a>
+                  </Link>
                 </div>
-                <a
+                <Link
                   href=""
                   class="box-border caret-transparent inline min-h-0 min-w-0 md:block md:min-h-[auto] md:min-w-[auto]"
                 >
                   Status Page
-                </a>
-                <a
+                </Link>
+                <Link
                   href="/terms"
                   class="box-border caret-transparent inline min-h-0 min-w-0 md:block md:min-h-[auto] md:min-w-[auto]"
                 >
                   Terms of Service
-                </a>
+                </Link>
                 <div class="box-border caret-transparent min-h-0 min-w-0 md:min-h-[auto] md:min-w-[auto]"></div>
               </div>
             </div>
